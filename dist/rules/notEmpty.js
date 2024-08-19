e59cd3c9404c85a16e2a375870d88a33e11b5371
Shiv Kushwaha <kushwaha.shiv@outlook.com>
2024-08-18 22:03:40 -0700

    yarn updated

--- dist/rules/notEmpty.js
+++ dist/rules/notEmpty.js
@@ -7,7 +7,6 @@ exports.default = void 0;
 
 var _ramda = require("ramda");
 
-// @flow
 var _default = (0, _ramda.complement)(_ramda.isEmpty);
 
 exports.default = _default;
--- src/rules/notEmpty.js
+++ src/rules/notEmpty.js
@@ -1,5 +1,3 @@
-// @flow
-
 import { isEmpty, complement } from 'ramda';
 
 export default complement(isEmpty);
